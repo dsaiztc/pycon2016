79b5f07bdf122e62ed2bb7356072c27f3fd08da4
Daniel Saiz <dsaiztc@gmail.com>
2016-11-03 21:04:38 +0000

    Reading events as JSON

--- src/main.py
+++ src/main.py
@@ -38,6 +38,58 @@ def get_schedule_html():
 
 def main():
     schedule_html = get_schedule_html()
+    soup = BeautifulSoup(schedule_html, 'html.parser')
+
+    day_elements = soup.select('p.schedule__day-switcher-item')
+    days = [ {'name': day.string, 'id': day['data-date'] } for day in day_elements]
+
+    for day in days:
+        day['events'] = []
+        day_schedule = soup.select('#{id}'.format(id=day['id']))[0]
+        day_event_elements = day_schedule.select('div.session')
+        for day_event_element in day_event_elements:
+
+            event = {
+                'time_start': day_event_element.select('span.session__time')[0].string.replace('\n', '').strip(),
+                'id': day_event_element.select('div.session__content')[0]['data-link'],
+                'title': day_event_element.select('h5.session__title')[0].string
+            }
+
+            event['datetime_start'] = datetime.datetime.strptime('2016' + day['name'] + event['time_start'], '%Y%d %B%I:%M %p')
+            event['datetime_start_str'] = str(event['datetime_start'])
+
+            event_description_element = soup.select('#{id}'.format(id=event['id']))[0]
+            event_time_finish = event_description_element.select('span.session__time')[1].string.replace('\n', '').strip()
+            event['time_finish'] = event_time_finish
+            event['datetime_finish'] = datetime.datetime.strptime('2016' + day['name'] + event['time_finish'], '%Y%d %B%I:%M %p')
+            event['datetime_finish_str'] = str(event['datetime_finish'])
+
+            event['location'] = event_description_element.select('span.session__location')[0].string if len(event_description_element.select('span.session__location')) > 0 else ''
+            event['description'] = event_description_element.select('div.uv-card__description')[0].string if len(event_description_element.select('div.uv-card__description')) > 0 else ''
+            event['description'] = event['description'].replace('\n', '').strip() if event['description'] else ''
+            if not event['description']:
+                event['description'] = ''.join(event_description_element.select('div.uv-card__description')[0].stripped_strings) if len(event_description_element.select('div.uv-card__description')) > 0 else ''
+
+            if event['title'] == 'Introduction to Pandas library':
+                print(event_description_element.select('div.uv-card__description'))
+                print(event_description_element.select('div.uv-card__description')[0].string)
+                strings = event_description_element.select('div.uv-card__description')[0].stripped_strings
+                for string in strings:
+                    print(string)
+                    print('\n')
+                print(''.join(event_description_element.select('div.uv-card__description')[0].stripped_strings))
+
+            speakers_list = []
+            event_speakers_elements = event_description_element.select('div.uv-shortcard--speaker')
+            for event_speakers_element in event_speakers_elements:
+                speaker_details = []
+                speaker_details.append(event_speakers_element.select('div.uv-shortcard__title')[0].string)
+                for subtitle in event_speakers_element.select('div.uv-shortcard__subtitle'):
+                    speaker_details.append(subtitle.string)
+                speakers_list.append(' - '.join(speaker_details))
+            event['speakers'] = '; '.join(speakers_list)
+
+            day['events'].append(event)
 
 
 if __name__ == '__main__':
